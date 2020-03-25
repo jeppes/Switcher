761fbacd0311c4a285c4b80de2a7c700dccf611e
Jesper Sandström <jespersandstrom@gmail.com>
2020-03-25 22:49:08 +0100

    Initial Commit

---
+++ Switcher/Switcher.h
@@ -0,0 +1,19 @@
+//
+//  Switcher.h
+//  Switcher
+//
+//  Created by Jesper Sandstrom on 3/25/20.
+//  Copyright © 2020 jeppes. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Switcher.
+FOUNDATION_EXPORT double SwitcherVersionNumber;
+
+//! Project version string for Switcher.
+FOUNDATION_EXPORT const unsigned char SwitcherVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Switcher/PublicHeader.h>
+
+
